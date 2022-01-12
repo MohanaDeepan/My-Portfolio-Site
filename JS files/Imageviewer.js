ced40db8b2b212ef42c45dee8f425b2592d9b4c5
Mohana Deepan <59493666+MohanaDeepan@users.noreply.github.com>
2022-01-13 02:26:19 +0530

    Add files via upload

---
+++ JS files/Imageviewer.js
@@ -0,0 +1,8 @@
+$(".pic img").click(function () {
+  $("#full-image").attr("src", $(this).attr("src"));
+  $("#image-viewer").show();
+});
+
+$("#image-viewer .close").click(function () {
+  $("#image-viewer").hide();
+});
